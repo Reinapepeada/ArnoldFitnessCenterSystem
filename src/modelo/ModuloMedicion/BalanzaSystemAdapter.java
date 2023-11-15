feaae4b0844ef96622fe9bc7cd6dd00547b304b9
Agustin Aused <agus.aused@gmail.com>
2023-11-15 12:24:02 -0300

    Implemented ModuloMedicion package for measurement functionalities

---
+++ src/modelo/ModuloMedicion/BalanzaSystemAdapter.java
@@ -0,0 +1,10 @@
+package modelo.ModuloMedicion;
+
+public class BalanzaSystemAdapter implements AdapterMedida {
+
+    public void tomarMedidas(){
+        // TODO - implement BalanzaSystemAdapter.tomarMedidas
+        
+    }
+    
+}
