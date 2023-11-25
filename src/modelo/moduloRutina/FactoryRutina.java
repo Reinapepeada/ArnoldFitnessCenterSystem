ca0b6100b98d7f9d19f9b173e133e238c8fe8331
Joaquín Martínez <joaquinuriel@icloud.com>
2023-11-25 17:11:25 +0000

    Refactor FactoryRutina to fix import and formatting issues

--- src/modelo/moduloRutina/FactoryRutina.java
+++ src/modelo/moduloRutina/FactoryRutina.java
@@ -4,32 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 import modelo.Entrenamiento;
+import modelo.Excepciones.RutinaInexistenteException;
 import modelo.enums.Dia;
-import modelo.excepciones.RutinaInexistenteException;
 import modelo.moduloObjetivo.ObjetivoStrategy;
 
 public class FactoryRutina {
-    public static Rutina crearRutina(ObjetivoStrategy objetivo,List<Dia> dias) throws RutinaInexistenteException {
+    public static Rutina crearRutina(ObjetivoStrategy objetivo, List<Dia> dias) throws RutinaInexistenteException {
         switch (objetivo.getClass().getSimpleName()) {
             case "Tonificar":
-            ArrayList<Entrenamiento> entrenamientos = new ArrayList();
+                ArrayList<Entrenamiento> entrenamientos = new ArrayList<>();
                 for (int i = 0; i < dias.size(); i++) {
-                    //por cada iteracion crear un entrenamiento con cada grupo muscular 
+                    // por cada iteracion crear un entrenamiento con cada grupo muscular
                     Entrenamiento entrenamiento = new Entrenamiento(objetivo);
                     entrenamientos.add(entrenamiento);
                 }
-                Rutina rut = new Rutina(objetivo,dias);
+                Rutina rut = new Rutina(objetivo, dias);
                 return rut;
-            case "BajarPeso":
-                Rutina ruti = new Rutina(objetivo,dias);
-                return ruti;
-            case "Mantener":
-                
-                Rutina rutn = new Rutina(objetivo,dias);
+                case "BajarPeso":
+                    Rutina ruti = new Rutina(objetivo, dias);
+                    return ruti;
+                case "Mantener":
+                    Rutina rutn = new Rutina(objetivo, dias);
                 return rutn;
             default:
                 throw new RutinaInexistenteException("No existe la Rutina");
         }
     }
-    
+
 }
